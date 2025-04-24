a84edeeef4eb478367cdf73ed3d1f1c1e39a04bf
Santiago Carmena <sca0017@alu.medac.es>
2025-04-24 12:53:58 +0200

    feat: menu con enlaces dinámico y vistas de usuario alumno

--- b25_conv_electro/src/app/components/dashboard/student/student.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-student',
-  imports: [],
-  templateUrl: './student.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class StudentComponent { }

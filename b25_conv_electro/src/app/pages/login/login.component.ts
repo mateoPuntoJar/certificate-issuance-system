b0a74a530e8c5380132d7d5e62dd6e3f4572522e
Miguel Ángel López García <mlg0046@alu.medac.es>
2025-04-24 08:56:13 +0200

    Eliminado check in y movido login a la carpeta page

--- b25_conv_electro/src/app/pages/login/login.component.ts
+++ b25_conv_electro/src/app/pages/login/login.component.ts
@@ -1,12 +1,30 @@
+import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { LogInComponent } from "../../components/login/log-in/log-in.component";
-import { CheckInComponent } from "../../components/login/check-in/check-in.component";
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LogInComponent, CheckInComponent],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent { }
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado: ', this.loginForm.value);
+      //Crear llamada al servicio de Supabase aquí
+    } else {
+      // Muestra errores si los hay
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+}

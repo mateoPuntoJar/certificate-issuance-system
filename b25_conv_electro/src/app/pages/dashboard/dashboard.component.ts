66a843d446f5ed48bc424620b4de76a9b8955e2c
thejorgelanz7 <jlm0039@alu.medac.es>
2025-04-30 19:04:58 +0200

    Condicional para mostrar el menu segun estudiante o administrador

--- b25_conv_electro/src/app/pages/dashboard/dashboard.component.ts
+++ b25_conv_electro/src/app/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { StudentMenuComponent } from '../../shared/components/student-menu/student-menu.component';
 import { AdminComponent, User } from '../../components/dashboard/admin/admin.component';
 import { AdminMenuComponent } from "../../shared/components/admin-menu/admin-menu.component";
+import { RouterOutlet } from '@angular/router';
 import { NotificationBannerComponent } from '../../shared/components/notifications-banner/notifications.banner.component';
+import { AuthService } from '../../supabase/auth.service';
+import { Router } from 'express';
+import { get } from 'http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, StudentMenuComponent, AdminComponent, AdminMenuComponent, NotificationBannerComponent,],
+  imports: [CommonModule, StudentMenuComponent, AdminComponent, AdminMenuComponent, NotificationBannerComponent,RouterOutlet],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent { 
+
+
+export class DashboardComponent  implements OnInit{ 
+  constructor(public authService : AuthService){}
+  ngOnInit(): void {
+    this.getRol();
+  }
+  
+  rol : string = ""
+  
+  
+  async getRol(){
+    this.rol = await this.authService.userRol
+    return this.rol
+  }
+  
+
   estudiantes: User[] = [
   ];
+
 }

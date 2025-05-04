5be8084a2cf393e020504b6223615a740594ff51
Santiago Carmena <sca0017@alu.medac.es>
2025-05-04 21:40:15 +0200

    feat: conexión a la BD y funcionalidad en profile, form, y status

--- b25_conv_electro/src/app/components/dashboard/student/status/status.component.ts
+++ b25_conv_electro/src/app/components/dashboard/student/status/status.component.ts
@@ -1,13 +1,39 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SupabaseService } from '../../../../supabase/supabase.service';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-status',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './status.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatusComponent {
-  status: 'enviado' | 'pendiente' | 'aprobado' | 'suspendido' = 'enviado';
-  comentarioAdmin: string = 'Por favor, adjunta también tu certificado de manipulador de alimentos.';
+export class StatusComponent implements OnInit {
+  documentos: any[] = [];
+
+  constructor(
+    private supabase: SupabaseService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  async ngOnInit() {
+    const { data } = await this.supabase.client.auth.getSession();
+    const uid = data.session?.user?.id;
+    if (uid) await this.loadDocs(uid);
+
+    this.supabase.client.auth.onAuthStateChange((_event, session) => {
+      const newUid = session?.user?.id;
+      if (newUid && this.documentos.length === 0) this.loadDocs(newUid);
+    });
+  }
+
+  async loadDocs(uid: string) {
+    const { data } = await this.supabase.client
+      .from('documentos_subidos')
+      .select('*')
+      .eq('uid_usuario', uid);
+
+    if (data) this.documentos = data;
+    this.cdr.detectChanges();
+  }
 }
